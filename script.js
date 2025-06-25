343b8f8d20d02fd958509211d90068af0860910f
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-06-25 06:14:12 +0500

    done with this project

--- script.js
+++ script.js
@@ -41,23 +41,42 @@ const game = {
   },
 };
 
+// feature no 1
 const [players1, players2] = game.players;
 console.log(players1, players2);
+console.log(players1);
 
+// feature no 2 to separate the gk and all the other players
 const [gk, ...fieldPlayers] = players1;
-// const [gk, ...fieldPlayers] = [players2];
 console.log(gk, fieldPlayers);
 
-const allPlayers = [...players1, players2];
+// feature no 3 to place all the players in an one team
+const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
+// feature no 4 adding the new players to the team 1 that got substitutued in the match
 const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 console.log(players1Final);
 
-const { team1, x: draw, team2 } = game.odds;
-console.log(game.odds);
+// feature no 5 accessing the odds from the game
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// feature no 6 goal scoreres from all the players of the match and to collect them in an single arrat
 
 const printGoals = function (...players) {
-  return console.log(`${players.length} goals were scored.`);
+  console.log(`${players.length} goals were scored`);
 };
-printGoals("shery");
+
+// printGoals("Davies", "Muller", "Lewandowski");
+
+printGoals(...game.scored);
+// console.log(...game.scored);
+
+// feature no 7 tells which team will win based on the odds
+
+team1 < team2 && console.log("team 1 is more likely to win");
+team1 > team2 && console.log("team 2 is more likely to win");
